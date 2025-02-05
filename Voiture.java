d5a29de702a1bde08efe088c7b34bbc9ba518513
Kanounou <kanoutecoumba00@gmail.com>
2025-02-05 15:27:22 +0100

    Premier commit

---
+++ Voiture.java
@@ -0,0 +1,17 @@
+public class Voiture {
+    //Attributs
+     String marque;
+     String modele;
+     String couleur;
+    //Methodes
+    void demarrer(){
+        System.out.println("La voiture demarre");
+    }
+    void accelerer(){
+        System.out.println("La voiture accelere");
+    }
+    void freiner(){
+        System.out.println("La voiture freine");
+    }
+    
+}
---
+++ main.java
@@ -0,0 +1,16 @@
+public class main {
+
+    public static void main(String[] args) {
+        //*creation de la voiture poo et la demarrer*/
+        
+        Voiture voiture = new Voiture();
+        voiture.marque = "Peugeot";
+        voiture.modele = "Clio";
+        voiture.couleur = "Rouge";
+
+        voiture.demarrer();
+         
+
+    }
+
+}
